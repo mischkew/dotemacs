7d7d7a08e93959dfd91b294f69ebb811adb968f0
Sven Mkw <sven.mkw@gmail.com>
2017-07-21 19:36:50 +0200

    added windmove keybindings

--- my-emacs-init.el
+++ my-emacs-init.el
@@ -255,4 +255,10 @@
 ;; kill the current buffer
 (global-set-key (kbd "C-x C-k") #'kill-current-buffer)
 
+;; windmove
+(global-set-key (kbd "C-c b") 'windmove-left)
+(global-set-key (kbd "C-c f") 'windmove-right)
+(global-set-key (kbd "C-c p") 'windmove-up)
+(global-set-key (kbd "C-c n") 'windmove-down)
+
 ;;; my-emacs-init.el ends here
