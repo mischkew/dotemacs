d146ed97bba6421a6141955402d6ac2005bbe896
Sven Mkw <sven.mkw@gmail.com>
2017-07-21 19:29:07 +0200

    initial commit
    setting up emacs with
    - use-package initialization
    - python mode / elpy
    - markdown mode
    - dockerfile mode
    - powerline themes
    - github theme

---
+++ init.el
@@ -0,0 +1,94 @@
+;; init.el --- My personal Emacs configuration.
+;;
+;; Copyright (c) 2015, 2016
+;;
+;; Author: Jonathan Chu <me@jonathanchu.is>
+;; URL: https://github.com/jonathanchu/dotemacs
+;; Version: 1.0
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; This is the whole #!
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+(setq user-full-name "Sven Mischkewitz"
+      user-mail-address "sven.mkw@gmail.com")
+
+(add-to-list 'exec-path "/usr/local/bin")
+(add-to-list 'exec-path "/usr/local/sbin")
+
+(defvar current-user
+  (getenv
+   (if (equal system-type 'windows-nt) "USERNAME" "USER")))
+
+(message "Your Emacs is powering up... Be patient, Master %s!" current-user)
+
+;;----------------------------------------------------------------------------
+;; Packages
+;;----------------------------------------------------------------------------
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")))
+
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; use-package
+(eval-when-compile
+  (require 'use-package))
+(setq use-package-verbose t)
+(require 'diminish)                ;; if you use :diminish
+(require 'bind-key)                ;; if you use any :bind variant
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("9b1c580339183a8661a84f5864a6c363260c80136bd20ac9f00d7e1d662e936a" "a94f1a015878c5f00afab321e4fef124b2fc3b823c8ddd89d360d710fc2bddfc" "b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;----------------------------------------------------------------------------
+;; Literate Config Test
+;;----------------------------------------------------------------------------
+
+(defvar my-init-file (expand-file-name "my-emacs-init.el" user-emacs-directory)
+  "Test configurations stored in this file.")
+
+(if (file-exists-p my-init-file)
+        (load-file my-init-file))
+
+;;; init.el ends here
---
+++ my-emacs-init.el
@@ -0,0 +1,258 @@
+;; my-emacs-init.el --- My personal Emacs configuration.
+
+;;; Code:
+
+;;
+;; -- General Stuff --
+;;
+
+;; github theme (light)
+(use-package github-theme
+  :ensure t)
+
+;; doom theme (dark)
+;; TODO
+
+;; set theme depending on your mood
+(load-theme 'github t)
+
+;; Set the number to the number of columns to use.
+(setq-default fill-column 79)
+
+;; Add Autofill mode to mode hooks.
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
+
+;; Show line number in the mode line.
+(line-number-mode 1)
+
+;; Show column number in the mode line.
+(column-number-mode 1)
+
+;; Enable syntax highlighting. This will also highlight lines that
+;; form a region.
+(global-font-lock-mode 1)
+
+;; Always load newest byte code
+(setq load-prefer-newer t)
+
+;; Warn when opening files bigger than 100MB
+(setq large-file-warning-threshold 100000000)
+
+;; Reduce the frequency of garbage collection by making it happen on
+;; each 25MB of allocated data (the default is on every 0.76MB)
+(setq gc-cons-threshold 25000000)
+
+;; only type 'y' or 'n' instead of 'yes' or 'no'
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; no splash screen
+(setq inhibit-splash-screen t)
+
+;; no message on startup
+(setq initial-scratch-message nil)
+
+;; no menu bar
+(menu-bar-mode -1)
+
+;; no toolbar
+(when (functionp 'tool-bar-mode)
+  (tool-bar-mode -1))
+  
+;; disable scroll bars
+(if window-system
+    (progn
+      (scroll-bar-mode -1)))
+
+;; set default cursor type
+(setq-default cursor-type 'bar)
+
+;; nice fonts in OS X
+(setq mac-allow-anti-aliasing t)
+
+;; highlight current line
+(global-hl-line-mode +1)
+
+;; show file size
+(size-indication-mode t)
+
+;; show extra whitespace
+(setq show-trailing-whitespace t)
+
+;; ensure last line is a return
+(setq require-final-newline t)
+
+;; set encoding
+(prefer-coding-system 'utf-8)
+
+;; and tell emacs to play nice with encoding
+(define-coding-system-alias 'UTF-8 'utf-8)
+(define-coding-system-alias 'utf8 'utf-8)
+
+;;keep cursor at same position when scrolling
+(setq scroll-preserve-screen-position t)
+
+;; scroll one line at a time
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 1) ;; keyboard scroll one line at a time
+(setq scroll-conservatively 10000)
+(setq scroll-margin 3)
+
+;; instantly display current key sequence in mini buffer
+(setq echo-keystrokes 0.02)
+
+;; improve filename completion
+(setq read-file-name-completion-ignore-case t)
+(setq read-buffer-completion-ignore-case t)
+(mapc (lambda (x)
+	(add-to-list 'completion-ignored-extensions x))
+      '(".gz" ".pyc" ".elc" ".exe"))
+
+;; useful for camelCase
+(subword-mode t)
+
+;; delete selection, insert text
+(delete-selection-mode t)
+
+;; write temp files to directory to not clutter the filesystem
+(defvar user-temporary-file-directory
+  (concat temporary-file-directory user-login-name "/"))
+(make-directory user-temporary-file-directory t)
+(setq backup-by-copying t)
+(setq backup-directory-alist
+      `(("." . ,user-temporary-file-directory)
+	(,tramp-file-name-regexp nil)))
+(setq auto-save-list-file-prefix
+      (concat user-temporary-file-directory ".auto-saves-"))
+(setq auto-save-file-name-transforms
+      `((".*" ,user-temporary-file-directory t)))
+
+;; start server in window mode
+(when (display-graphic-p)
+  (unless (and (fboundp 'server-running-p)
+             (server-running-p))
+    (server-start)))
+
+;;
+;; -- Minor Modes --
+;;
+
+;; save sessions and buffers
+(desktop-save-mode 1)
+(setq desktop-restore-eager 5)
+
+;; improve emacs bottom line
+(use-package airline-themes
+  :ensure t
+  :config
+  (load-theme 'airline-papercolor))
+
+;; Parenthesis Highlighting
+(use-package paren
+  :config
+    (show-paren-mode t))
+
+;; support cross-editor code formatting using .editorconfig files
+(use-package editorconfig
+  :ensure t
+  :config
+  (editorconfig-mode 1))
+
+;; emacs python ide
+(use-package elpy
+  :ensure t
+  :config
+  (elpy-enable))
+
+;; fringe
+(use-package fringe-helper
+  :ensure t)
+
+(setq-default fringes-outside-margins t
+              highlight-nonselected-windows nil)
+
+;; git highlights
+(use-package git-gutter-fringe
+  :ensure t)
+
+(use-package git-gutter
+  :ensure t
+  :config
+  (require 'git-gutter-fringe)
+  (global-git-gutter-mode +1)
+  (add-hook 'focus-in-hook 'git-gutter:update-all-windows))
+
+;;
+;; -- Major Modes --
+;;
+
+;; edit Dockerfiles
+(use-package dockerfile-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode)))
+
+;; Markdown files with Github flavor
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+	 ("\\.md\\'" . markdown-mode)
+	 ("\\.markdown\\'" . markdown-mode))
+    :init (setq markdown-command "multimarkdown"))
+
+;; Python major mode
+(use-package python-mode
+  :ensure t
+  :config
+  (add-hook 'python-mode-hook
+            '(lambda ()
+               (setq fill-column 80)))
+  (add-to-list 'auto-mode-alist '("\\.py" . python-mode)))
+
+;; Git with emacs
+(use-package magit
+  :ensure t
+  :config
+  (progn
+    (setq magit-push-always-verify nil)
+    (setq magit-completing-read-function #'ivy-completing-read)
+    (setq magit-last-seen-setup-instructions "1.4.0")
+    (setq magit-diff-refine-hunk t))
+  :bind
+  ("C-x C-g" . magit-status)
+  ("C-c C-a" . magit-commit-amend))
+
+;;
+;; -- Function --
+;;
+
+;; duplicate the current line function
+(defun duplicate-line ()
+  "Duplicate the current line."
+  (interactive)
+  (move-beginning-of-line 1)
+  (kill-line)
+  (yank)
+  (open-line 1)
+  (forward-line 1)
+  (yank))
+
+;; Kill the current buffer
+(defun kill-current-buffer ()
+  "Kills the current buffer"
+  (interactive)
+  (kill-buffer (buffer-name)))
+
+;;
+;; -- Key Bindings --
+;;
+
+;; duplicate the current line
+(global-set-key (kbd "C-c d") #'duplicate-line)
+
+;; kill the current buffer
+(global-set-key (kbd "C-x C-k") #'kill-current-buffer)
+
+;;; my-emacs-init.el ends here
