93e1b40f0fd76de06e6aa00e148bfff4a0c1b927
Sven Mischkewitz <sven.mkw@gmail.com>
2017-12-01 10:48:24 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,47 @@
+#! /bin/sh
+
+platform=$(uname -s)
+if [ $platform = "Darwin" ]; then IS_OSX="1"; fi
+
+user=$(whoami)
+CHECK=✔
+DONE="${CHECK} DONE."
+
+if [ -z "$IS_OSX" ]; then
+    BASEDIR=$(readlink -e $(dirname $0))
+else
+    echo "Not implemented for OSX"
+    exit 1
+fi
+echo "Running emacs install from $BASEDIR"
+
+
+link_emacs_to_emacsd() {
+    echo "Install emacs configuration files to $HOME/.emacs.d"
+    ln -i -v -s $BASEDIR $HOME/.emacs.d
+    echo "$DONE"
+}
+
+install_emacs() {
+    if [ -z $IS_OSX ]; then
+	install_emacs_linux
+    else
+	install_emacs_osx
+    fi
+}
+
+install_emacs_osx() {
+    echo "Not implemented. Exiting."
+    exit 1
+}
+
+install_emacs_linux() {
+    echo "Installing emacs package..."
+    sudo apt-get install emacs24
+    echo "$DONE"
+}
+
+install() {
+    install_emacs
+    link_emacs_to_emacsd
+}
